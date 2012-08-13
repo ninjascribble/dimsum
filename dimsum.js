e7cc44cac29ab74bfe501d8ef68dbc308744c09a
Scott Grogan <scott@ninjascript.com>
2012-08-12 19:01:53 -0700

    Adding bounds to config

--- dimsum.js
+++ dimsum.js
@@ -7,27 +7,29 @@
  */
 ;(function(global) {
 
-var dimsum = global.dimsum = module.exports = exports = {};
+var dimsum = global.dimsum = module.exports = exports = {},
+	
+	classic = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 
-var punct = [',','.',';',':','?'];
+	cicero_1_10_32 = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+
+	cicero_1_10_33 = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+
+	config = {
+		'format': 'text',
+		'bounds': {
+			'per_paragraph': {
+				sentences: [3, 5]
+			},
+			'per_sentence': {
+				words: [10, 31],
+				commas: [0, 4]
+			}
+		}
+	},
+
+	punct = [',','.',';',':','?'];
 	punct.regexp = new RegExp('[' + punct.join('') + ']*','g');
-
-var grammar = {
-	sentences_per_paragraph: [3,5],
-	questions_per_paragraph: [0,2],
-	words_per_sentence: [10,31],
-	commas_per_sentence: [0,4]
-};
-
-var config = {
-	'format': 'text'
-};
-
-var classic = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
-var cicero_1_10_32 = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
-
-var cicero_1_10_33 = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.";
 
 dimsum.latin = dedupe( normify([classic, cicero_1_10_32, cicero_1_10_33]).split(' ') );
 
@@ -39,10 +41,10 @@ dimsum.cicero33 = function() { return cicero_1_10_33; };
 
 dimsum.sentence = function() {
 
-	var word = '';
-	var words = [];
-	var num_words = range(grammar.words_per_sentence[0], grammar.words_per_sentence[1]);
-	var num_commas = range(grammar.commas_per_sentence[0], grammar.commas_per_sentence[1]);
+	var word = '',
+		words = [],
+		num_words = range(config.bounds.per_sentence.words[0], config.bounds.per_sentence.words[1]),
+		num_commas = range(config.bounds.per_sentence.commas[0], config.bounds.per_sentence.commas[1]);
 
 	// Get some words
 	while (words.length < num_words) {
@@ -70,8 +72,8 @@ dimsum.sentence = function() {
 };
 
 dimsum.paragraph = function() {
-	var sentences = [];
-	var num_sentences = range(grammar.sentences_per_paragraph[0], grammar.sentences_per_paragraph[1]);
+	var sentences = [],
+		num_sentences = range(config.bounds.per_paragraph.sentences[0], config.bounds.per_paragraph.sentences[1]);
 	while (sentences.length < num_sentences) {
 		sentences.push(this.sentence());
 	}
@@ -92,9 +94,9 @@ dimsum.configure = function(options) {
  * @param num_paragraphs How many paragraphs to generate.
  */
 dimsum.generate = function(num_paragraphs, options) {
-	var configuration = options || config;
-	var sentences = [];
-	var paragraphs = [];
+	var configuration = options || config,
+		sentences = [],
+		paragraphs = [];
 	num_paragraphs = num_paragraphs || 1;
 	while (paragraphs.length < num_paragraphs) {
 		paragraphs.push(this.paragraph());
@@ -117,8 +119,8 @@ function normify(strings) {
 };
 
 function dedupe(array) {
-	var obj = {};
-	var result = [];
+	var obj = {},
+		result = [];
 	for (var i = 0; i < array.length; i++) {
 		obj[array[i]] = null;
 	}
