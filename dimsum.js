038f9d9da5c701b807c591730d5edfc5916586f6
Scott Grogan <scott@ninjascript.com>
2012-08-12 19:33:16 -0700

    Some major cleanup work

--- dimsum.js
+++ dimsum.js
@@ -7,9 +7,7 @@
  */
 ;(function(global) {
 
-var dimsum = global.dimsum = module.exports = exports = {},
-	
-	classic = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+var classic = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 
 	cicero_1_10_32 = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
 
@@ -32,125 +30,161 @@ var dimsum = global.dimsum = module.exports = exports = {},
 
 	punct_reg = new RegExp('[' + punct.join('') + ']*','g'),
 
-	latin = dedupe( normify([classic, cicero_1_10_32, cicero_1_10_33]).split(' ') );
+	latin = dedupe( normify([classic, cicero_1_10_32, cicero_1_10_33]).split(' ') ),
 
-dimsum.classic = function() { return classic; };
+	dimsum = global.dimsum = module.exports = exports = {
 
-dimsum.cicero32 = function() { return cicero_1_10_32; };
+		/**
+		 * The standard Lorem Ipsum passage.
+		 */
+		classic: function() {
+			return classic;
+		},
 
-dimsum.cicero33 = function() { return cicero_1_10_33; };
+		/**
+		 * Section 1.10.32 of Cicero's "de Finibus Bonorum et Malorum".
+		 */
+		cicero32: function() {
+			return cicero_1_10_32;
+		},
 
-dimsum.sentence = function() {
+		/**
+		 * Section 1.10.33 of Cicero's "de Finibus Bonorum et Malorum".
+		 */
+		cicero33: function() {
+			return cicero_1_10_33;
+		},
 
-	var word = '',
-		words = [],
-		num_words = range(config.bounds.per_sentence.words[0], config.bounds.per_sentence.words[1]),
-		num_commas = range(config.bounds.per_sentence.commas[0], config.bounds.per_sentence.commas[1]);
+		/**
+		 * Override the current configuration.
+		 *
+		 * @param options
+		 *		An object containing new config values.
+		 */
+		configure: function(options) {
+			config = extend(config, options);
+			return this;
+		},
 
-	// Get some words
-	while (words.length < num_words) {
-		word = latin[ range(0, latin.length -1) ];
-		words.push(word);
-	}
+		/**
+		 * Create a random chunk of lipsum. Returns an array containing
+		 * the number of paragraphs specified.
+		 *
+		 * @param num_paragraphs
+		 * 		How many paragraphs to generate.
+		 *
+		 * @param options
+		 *		An object containing new config values. The new options
+		 *		will only apply to this one execution.
+		 */
+		generate: function(num_paragraphs, options) {
 
-	// Add some commas
-	for (var i = 0; i < num_commas; i++) {
-		word = range(4, words.length - 3);
-		if (words[word].match(',')) {
-			i--;
+			var config_1 = config,
+				sentences = [],
+				paragraphs = [],
+				result = '',
+				num_paragraphs = num_paragraphs || 1;
+
+			this.configure(options);
+
+			while (paragraphs.length < num_paragraphs) {
+				paragraphs.push(this.paragraph());
+			}
+
+			switch(config.format) {
+				case 'text':
+					result = paragraphs.join("\r\n\r\n");
+					break;
+				case 'html':
+					result = '<p>' + paragraphs.join('</p><p>') + '</p>';
+					break;
+			}
+
+			config = config_1;
+			return result;
+		},
+
+		/**
+		 * Create a single sentence of random lipsum.
+		 */
+		sentence: function() {
+
+			var word = '',
+				words = [],
+				num_words = range(config.bounds.per_sentence.words[0], config.bounds.per_sentence.words[1]),
+				num_commas = range(config.bounds.per_sentence.commas[0], config.bounds.per_sentence.commas[1]);
+
+			// Get some words
+			while (words.length < num_words) {
+				word = latin[ range(0, latin.length -1) ];
+				words.push(word);
+			}
+
+			// Add some commas
+			for (var i = 0; i < num_commas; i++) {
+				word = range(4, words.length - 3);
+				if (words[word].match(',')) {
+					i--;
+				}
+				else {
+					words[word] += ',';
+				}
+			}
+
+			// Capitalize the first word
+			words = words.join(' ');
+			words = words.replace(/^[a-z]/i, words[0].toUpperCase());
+
+			// Punctuate and return
+			return words + '.';
+		},
+
+		/**
+		 * Create a single paragraph of random lipsum.
+		 */
+		paragraph: function() {
+			var sentences = [],
+				num_sentences = range(config.bounds.per_paragraph.sentences[0], config.bounds.per_paragraph.sentences[1]);
+			while (sentences.length < num_sentences) {
+				sentences.push(this.sentence());
+			}
+			return sentences.join(' ');
 		}
-		else {
-			words[word] += ',';
+
+	};
+
+	/** Utils **/
+	function normify(strings) {
+		return strings.join(' ')
+				.toLowerCase()
+				.replace(punct_reg, '');
+	};
+
+	function dedupe(array) {
+		var obj = {},
+			result = [];
+		for (var i = 0; i < array.length; i++) {
+			obj[array[i]] = null;
 		}
-	}
-
-	// Capitalize the first word
-	words = words.join(' ');
-	words = words.replace(/^[a-z]/i, words[0].toUpperCase());
-
-	// Punctuate and return
-	return words + '.';
-};
-
-dimsum.paragraph = function() {
-	var sentences = [],
-		num_sentences = range(config.bounds.per_paragraph.sentences[0], config.bounds.per_paragraph.sentences[1]);
-	while (sentences.length < num_sentences) {
-		sentences.push(this.sentence());
-	}
-	return sentences.join(' ');
-};
-
-dimsum.configure = function(options) {
-	config = extend(config, options);
-	return this;
-};
-
-/**
- * Create a random chunk of lipsum. Returns an array containing
- * the number of paragraphs specified.
- *
- * @param num_paragraphs How many paragraphs to generate.
- */
-dimsum.generate = function(num_paragraphs, options) {
-
-	var config_1 = config,
-		sentences = [],
-		paragraphs = [],
-		result = '',
-		num_paragraphs = num_paragraphs || 1;
-
-	this.configure(options);
-
-	while (paragraphs.length < num_paragraphs) {
-		paragraphs.push(this.paragraph());
-	}
-
-	switch(config.format) {
-		case 'text':
-			result = paragraphs.join("\r\n\r\n");
-			break;
-		case 'html':
-			result = '<p>' + paragraphs.join('</p><p>') + '</p>';
-			break;
-	}
-
-	config = config_1;
-	return result;
-};
-
-/** Utils **/
-function normify(strings) {
-	return strings.join(' ')
-			.toLowerCase()
-			.replace(punct_reg, '');
-};
-
-function dedupe(array) {
-	var obj = {},
-		result = [];
-	for (var i = 0; i < array.length; i++) {
-		obj[array[i]] = null;
-	}
-	for (key in obj) {
-		result.push(key);
-	}
-	return result;
-}
-
-function range(min, max) {
-	return min + Math.random() * (max - min - 1) << 0;
-}
-
-function extend() {
-	var i, key, result = {},
-		args = Array.prototype.slice.call(arguments);
-	for (i = 0; i < args.length; i++) {
-		for (key in args[i]) {
-			result[key] = args[i][key];
+		for (key in obj) {
+			result.push(key);
 		}
+		return result;
 	}
-	return result;
-}
+
+	function range(min, max) {
+		return min + Math.random() * (max - min - 1) << 0;
+	}
+
+	function extend() {
+		var i, key, result = {},
+			args = Array.prototype.slice.call(arguments);
+		for (i = 0; i < args.length; i++) {
+			for (key in args[i]) {
+				result[key] = args[i][key];
+			}
+		}
+		return result;
+	}
 
 }(this));
