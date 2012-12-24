6f1d02ab8122533af8cb648c68cbd5075594fb62
Scott Grogan <scott@ninjascript.com>
2012-12-24 14:49:51 -0800

    Converting everything to spaces instead of tabs

--- dimsum.js
+++ dimsum.js
@@ -14,11 +14,11 @@ cicero_1_10_32 = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
 cicero_1_10_33 = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
 
 defaults = {
-	'format': 'text',
-	'flavor': 'latin',
-	'sentences_per_paragraph': [3, 5],
-	'words_per_sentence': [10, 31],
-	'commas_per_sentence': [0, 4]
+    'format': 'text',
+    'flavor': 'latin',
+    'sentences_per_paragraph': [3, 5],
+    'words_per_sentence': [10, 31],
+    'commas_per_sentence': [0, 4]
 },
 
 config = {},
@@ -28,11 +28,11 @@ punct = [',','.',';',':','?','!'],
 punct_reg = new RegExp('[' + punct.join('') + ']*','g'),
 
 flavors = {
-	'latin': dedupe( normify([classic, cicero_1_10_32, cicero_1_10_33]).split(' ') )
+    'latin': dedupe( normify([classic, cicero_1_10_32, cicero_1_10_33]).split(' ') )
 },
 
 dimsum = global.dimsum = exports = function(num_paragraphs, options) {
-	return dimsum.generate(num_paragraphs, options);
+    return dimsum.generate(num_paragraphs, options);
 };
 
 // For client-side compatibility
@@ -42,46 +42,46 @@ if (typeof module !== 'undefined') module.exports = dimsum;
  * The standard Lorem Ipsum passage.
  */
 dimsum.classic = function() {
-	return classic;
+    return classic;
 };
 
 /**
  * Section 1.10.32 of Cicero's "de Finibus Bonorum et Malorum".
  */
 dimsum.cicero32 = function() {
-	return cicero_1_10_32;
+    return cicero_1_10_32;
 };
 
 /**
  * Section 1.10.33 of Cicero's "de Finibus Bonorum et Malorum".
  */
 dimsum.cicero33 = function() {
-	return cicero_1_10_33;
+    return cicero_1_10_33;
 };
 
 /**
  * Restore default configuration.
  */
 dimsum.initialize = function() {
-	return this.configure(defaults);
+    return this.configure(defaults);
 };
 
 /**
  * Override the current configuration.
  *
  * @param options
- *		An object containing new config values.
+ *      An object containing new config values.
  */
 dimsum.configure = function(options) {
-	config = shallow_copy(config, options);
-	return this;
+    config = shallow_copy(config, options);
+    return this;
 };
 
 /**
  * @return A list of flavors that are currently available.
  */
 dimsum.flavors = function() {
-	return Object.keys(flavors);
+    return Object.keys(flavors);
 };
 
 /**
@@ -91,22 +91,22 @@ dimsum.flavors = function() {
  */
 dimsum.flavor = function(name, ingredients) {
 
-	if (arguments.length == 0) {
-		return;
-	}
-	else if (arguments.length == 1) {
-		
-		if (name.match(/\s/)) {
-			return dedupe( normify([name]).split(' ') );
-		}
-		else {
-			return flavors[name];
-		}
-	}
-	else {
-		flavors[name] = (typeof ingredients == 'Array') ? ingredients : dedupe( normify([ingredients]).split(' ') );
-		return flavors[name];
-	}
+    if (arguments.length == 0) {
+        return;
+    }
+    else if (arguments.length == 1) {
+        
+        if (name.match(/\s/)) {
+            return dedupe( normify([name]).split(' ') );
+        }
+        else {
+            return flavors[name];
+        }
+    }
+    else {
+        flavors[name] = (typeof ingredients == 'Array') ? ingredients : dedupe( normify([ingredients]).split(' ') );
+        return flavors[name];
+    }
 };
 
 /**
@@ -114,42 +114,42 @@ dimsum.flavor = function(name, ingredients) {
  * the number of paragraphs specified.
  *
  * @param num_paragraphs
- * 		How many paragraphs to generate.
+ *      How many paragraphs to generate.
  *
  * @param options
- *		An object containing new config values. The new options
- *		will only apply to this one execution.
+ *      An object containing new config values. The new options
+ *      will only apply to this one execution.
  */
 dimsum.generate = function(num_paragraphs, options) {
 
-	var config_1 = config,
-		sentences = [],
-		paragraphs = [],
-		result = '',
-		num_paragraphs = num_paragraphs || 1;
+    var config_1 = config,
+        sentences = [],
+        paragraphs = [],
+        result = '',
+        num_paragraphs = num_paragraphs || 1;
 
-	// Temporarily overwrite the configuration
-	this.configure(options);
+    // Temporarily overwrite the configuration
+    this.configure(options);
 
-	// Add some paragraphs
-	while (paragraphs.length < num_paragraphs) {
-		paragraphs.push(this.paragraph());
-	}
+    // Add some paragraphs
+    while (paragraphs.length < num_paragraphs) {
+        paragraphs.push(this.paragraph());
+    }
 
-	// Make it pretty
-	switch(config.format) {
-		case 'text':
-			result = paragraphs.join("\r\n\r\n");
-			break;
-		case 'html':
-			result = '<p>' + paragraphs.join('</p><p>') + '</p>';
-			break;
-	}
+    // Make it pretty
+    switch(config.format) {
+        case 'text':
+            result = paragraphs.join("\r\n\r\n");
+            break;
+        case 'html':
+            result = '<p>' + paragraphs.join('</p><p>') + '</p>';
+            break;
+    }
 
-	// Restore the original configuration
-	config = config_1;
+    // Restore the original configuration
+    config = config_1;
 
-	return result;
+    return result;
 };
 
 /**
@@ -157,38 +157,38 @@ dimsum.generate = function(num_paragraphs, options) {
  */
 dimsum.sentence = function() {
 
-	var word = '',
-		words = [],
-		num_words = range(config.words_per_sentence[0], config.words_per_sentence[1]),
-		num_commas = range(config.commas_per_sentence[0], config.commas_per_sentence[1]),
-		flavor = flavors[config.flavor];
+    var word = '',
+        words = [],
+        num_words = range(config.words_per_sentence[0], config.words_per_sentence[1]),
+        num_commas = range(config.commas_per_sentence[0], config.commas_per_sentence[1]),
+        flavor = flavors[config.flavor];
 
-	// Get some words
-	while (words.length < num_words) {
-		word = flavor[ range(0, flavor.length -1) ];
-		words.push(word);
-	}
+    // Get some words
+    while (words.length < num_words) {
+        word = flavor[ range(0, flavor.length -1) ];
+        words.push(word);
+    }
 
-	// Add some commas
-	for (var i = 0; i < num_commas; i++) {
-		word = range(4, words.length - 3);
-		if (!words[word]) {
-			break;
-		}
-		else if (words[word].match(',')) {
-			i--;
-		}
-		else {
-			words[word] += ',';
-		}
-	}
+    // Add some commas
+    for (var i = 0; i < num_commas; i++) {
+        word = range(4, words.length - 3);
+        if (!words[word]) {
+            break;
+        }
+        else if (words[word].match(',')) {
+            i--;
+        }
+        else {
+            words[word] += ',';
+        }
+    }
 
-	// Capitalize the first word
-	words = words.join(' ');
-	words = words.replace(/^[a-z]/i, words[0].toUpperCase());
+    // Capitalize the first word
+    words = words.join(' ');
+    words = words.replace(/^[a-z]/i, words[0].toUpperCase());
 
-	// Punctuate and return
-	return words + '.';
+    // Punctuate and return
+    return words + '.';
 };
 
 /**
@@ -196,53 +196,53 @@ dimsum.sentence = function() {
  */
 dimsum.paragraph = function() {
 
-	var sentences = [],
-		num_sentences = range(config.sentences_per_paragraph[0], config.sentences_per_paragraph[1]);
+    var sentences = [],
+        num_sentences = range(config.sentences_per_paragraph[0], config.sentences_per_paragraph[1]);
 
-	while (sentences.length < num_sentences) {
-		sentences.push(this.sentence());
-	}
-	return sentences.join(' ');
+    while (sentences.length < num_sentences) {
+        sentences.push(this.sentence());
+    }
+    return sentences.join(' ');
 };
 
 dimsum.initialize();
 
 /** Utils **/
 function normify(strings) {
-	return strings.join(' ')
-			.toLowerCase()
-			.replace(punct_reg, '');
+    return strings.join(' ')
+            .toLowerCase()
+            .replace(punct_reg, '');
 };
 
 function dedupe(array) {
 
-	var obj = {},
-		result = [];
+    var obj = {},
+        result = [];
 
-	for (var i = 0; i < array.length; i++) {
-		obj[array[i]] = null;
-	}
-	for (key in obj) {
-		result.push(key);
-	}
-	return result;
+    for (var i = 0; i < array.length; i++) {
+        obj[array[i]] = null;
+    }
+    for (key in obj) {
+        result.push(key);
+    }
+    return result;
 };
 
 function range(min, max) {
-	return min + Math.random() * (max - min) << 0;
+    return min + Math.random() * (max - min) << 0;
 };
 
 function shallow_copy() {
 
-	var i, key, result = {},
-		args = Array.prototype.slice.call(arguments);
+    var i, key, result = {},
+        args = Array.prototype.slice.call(arguments);
 
-	for (i = 0; i < args.length; i++) {
-		for (key in args[i]) {
-			result[key] = args[i][key];
-		}
-	}
-	return result;
+    for (i = 0; i < args.length; i++) {
+        for (key in args[i]) {
+            result[key] = args[i][key];
+        }
+    }
+    return result;
 };
 
 /* Debug */
--- test/test.js
+++ test/test.js
@@ -1,201 +1,201 @@
 var dimsum, assert;
 
 if (typeof require == 'undefined') {
-	dimsum = dimsum;
-	assert = chai.assert;
+    dimsum = dimsum;
+    assert = chai.assert;
 }
-else {
-	dimsum = require('../dimsum');
-	assert = require('assert');
+else {  
+    dimsum = require('../dimsum');
+    assert = require('assert');
 }
 
 describe('dimsum', function() {
 
-	beforeEach(function() {
-		dimsum.initialize();
-	});
+    beforeEach(function() {
+        dimsum.initialize();
+    });
 
-	it('wraps the #generate() method', function() {
-		assert.ok(dimsum().match(/^[A-Z][A-Za-z.,\s]*.$/));
-	});
+    it('wraps the #generate() method', function() {
+        assert.ok(dimsum().match(/^[A-Z][A-Za-z.,\s]*.$/));
+    });
 
-	describe('#sentence()', function() {
+    describe('#sentence()', function() {
 
-		var c, i, s = [];
+        var c, i, s = [];
 
-		for (i = 0; i < 100; i++) {
-			s.push( dimsum.sentence() );
-		}
+        for (i = 0; i < 100; i++) {
+            s.push( dimsum.sentence() );
+        }
 
-		it('should start with a capital letter', function() {
-			for (i in s) {
-				c = s[i][0];
-				assert.equal(c, c.toUpperCase());	
-			}
-		});
+        it('should start with a capital letter', function() {
+            for (i in s) {
+                c = s[i][0];
+                assert.equal(c, c.toUpperCase());   
+            }
+        });
 
-		it('should end with a period', function() {
-			for (i in s) {
-				c = s[i][ s[i].length - 1 ];
-				assert.equal(c, '.');	
-			}
-		});
+        it('should end with a period', function() {
+            for (i in s) {
+                c = s[i][ s[i].length - 1 ];
+                assert.equal(c, '.');   
+            }
+        });
 
-		it('should never contain two or more punctuation characters in a row', function() {
-			for (i in s) {
-				assert.strictEqual(s[i].match(/[\.,]{2,}/g), null);
-			}
-		});
+        it('should never contain two or more punctuation characters in a row', function() {
+            for (i in s) {
+                assert.strictEqual(s[i].match(/[\.,]{2,}/g), null);
+            }
+        });
 
-	});
+    });
 
-	describe('#configure()', function() {
+    describe('#configure()', function() {
 
-		var d, i, o;
+        var d, i, o;
 
-		it('always returns dimsum', function() {
-			d = dimsum.configure({ 'format': 'text' });
-			assert.equal(d, dimsum);
-		});
+        it('always returns dimsum', function() {
+            d = dimsum.configure({ 'format': 'text' });
+            assert.equal(d, dimsum);
+        });
 
-		it('outputs plain text by default', function() {
-			o = dimsum.generate(3);
-			assert.equal(o.match(/<p>|<\/p>/g), null);
-			assert.equal(o.match(/\r\n\r\n/g).length, 2);
-		});
+        it('outputs plain text by default', function() {
+            o = dimsum.generate(3);
+            assert.equal(o.match(/<p>|<\/p>/g), null);
+            assert.equal(o.match(/\r\n\r\n/g).length, 2);
+        });
 
-		it('can output HTML text too!', function() {
-			dimsum.configure({ 'format': 'html' });
-			o = dimsum.generate(3);
-			assert.equal(o.match(/<p>/g).length, 3);
-			assert.equal(o.match(/<\/p>/g).length, 3);
-			assert.equal(o.match(/\r\n\r\n/g), null);
-		});
+        it('can output HTML text too!', function() {
+            dimsum.configure({ 'format': 'html' });
+            o = dimsum.generate(3);
+            assert.equal(o.match(/<p>/g).length, 3);
+            assert.equal(o.match(/<\/p>/g).length, 3);
+            assert.equal(o.match(/\r\n\r\n/g), null);
+        });
 
-		it('can adjust the number of sentences in a paragraph', function() {
-			dimsum.configure({ 
-				sentences_per_paragraph: [2,2]
-			});
-			for (i = 0; i < 100; i++) {
-				assert.strictEqual(dimsum.paragraph().match(/[\.]{1,1}/g).length, 2);
-			}
-		});
+        it('can adjust the number of sentences in a paragraph', function() {
+            dimsum.configure({ 
+                sentences_per_paragraph: [2,2]
+            });
+            for (i = 0; i < 100; i++) {
+                assert.strictEqual(dimsum.paragraph().match(/[\.]{1,1}/g).length, 2);
+            }
+        });
 
-		it('can adjust the number of words per sentence', function() {
-			dimsum.configure({ 
-				words_per_sentence: [1,1]
-			});
-			for (i = 0; i < 100; i++) {
-				assert.strictEqual(dimsum.sentence().split(' ').length, 1);
-			}
-		});
+        it('can adjust the number of words per sentence', function() {
+            dimsum.configure({ 
+                words_per_sentence: [1,1]
+            });
+            for (i = 0; i < 100; i++) {
+                assert.strictEqual(dimsum.sentence().split(' ').length, 1);
+            }
+        });
 
-		it('can adjust the number of commas per sentence', function() {
-			dimsum.configure({
-				commas_per_sentence: [1,1]
-			});
-			for (i = 0; i < 100; i++) {
-				assert.strictEqual(dimsum.sentence().match(/\,/g).length, 1);
-			}
-		});
+        it('can adjust the number of commas per sentence', function() {
+            dimsum.configure({
+                commas_per_sentence: [1,1]
+            });
+            for (i = 0; i < 100; i++) {
+                assert.strictEqual(dimsum.sentence().match(/\,/g).length, 1);
+            }
+        });
 
-	});
+    });
 
-	describe('#generate()', function() {
+    describe('#generate()', function() {
 
-		var p;
+        var p;
 
-		it('provides one paragraph of text by default', function() {
-			p = dimsum.generate();
-			assert.equal(p.match(/<p>|<\/p>/g), null);
-			assert.equal(p.match(/\r\n\r\n/g), null);
-		});
+        it('provides one paragraph of text by default', function() {
+            p = dimsum.generate();
+            assert.equal(p.match(/<p>|<\/p>/g), null);
+            assert.equal(p.match(/\r\n\r\n/g), null);
+        });
 
-		it('can override preconfigured options', function() {
-			dimsum.configure({ 'format': 'html' });
-			p = dimsum.generate(3, { 'format': 'text' });
-			assert.equal(p.match(/<p>|<\/p>/g), null);
-			assert.equal(p.match(/\r\n\r\n/g).length, 2);
-			p = dimsum.generate(3);
-			assert.equal(p.match(/<p>/g).length, 3);
-			assert.equal(p.match(/<\/p>/g).length, 3);
-			assert.equal(p.match(/\r\n\r\n/g), null);
-		});
+        it('can override preconfigured options', function() {
+            dimsum.configure({ 'format': 'html' });
+            p = dimsum.generate(3, { 'format': 'text' });
+            assert.equal(p.match(/<p>|<\/p>/g), null);
+            assert.equal(p.match(/\r\n\r\n/g).length, 2);
+            p = dimsum.generate(3);
+            assert.equal(p.match(/<p>/g).length, 3);
+            assert.equal(p.match(/<\/p>/g).length, 3);
+            assert.equal(p.match(/\r\n\r\n/g), null);
+        });
 
-	});
+    });
 
-	describe('#flavors()', function() {
+    describe('#flavors()', function() {
 
-		var result;
+        var result;
 
-		it('Returns a list of strings', function() {
-			result = dimsum.flavors();
-			assert.ok(result instanceof Array);
-			assert.equal(typeof result[0], 'string');
-		});
+        it('Returns a list of strings', function() {
+            result = dimsum.flavors();
+            assert.ok(result instanceof Array);
+            assert.equal(typeof result[0], 'string');
+        });
 
-		it('Only contains latin by default', function() {
-			result = dimsum.flavors();
-			assert.equal(result.length, 1);
-			assert.equal(result[0], 'latin');
-		});
+        it('Only contains latin by default', function() {
+            result = dimsum.flavors();
+            assert.equal(result.length, 1);
+            assert.equal(result[0], 'latin');
+        });
 
-	});
+    });
 
-	describe('#flavor()', function() {
+    describe('#flavor()', function() {
 
-		it('Returns an existing flavor when given a key', function() {
+        it('Returns an existing flavor when given a key', function() {
 
-			var classic = normify( [dimsum.classic()] ).split(' ')
-			  , result = dimsum.flavor('latin')
-			  , i = 0
-			  , len = classic.length;
+            var classic = normify( [dimsum.classic()] ).split(' ')
+              , result = dimsum.flavor('latin')
+              , i = 0
+              , len = classic.length;
 
-			for (i; i < len; i++) {
-				assert.ok(result.indexOf( classic[i] ) > -1);
-			}
+            for (i; i < len; i++) {
+                assert.ok(result.indexOf( classic[i] ) > -1);
+            }
 
-		});
+        });
 
-		it('Returns a new flavor when given a string', function() {
+        it('Returns a new flavor when given a string', function() {
 
-			var ingredients = "Space, the final frontier. These are the voyages of the starship Enterprise. Her five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before."
-			  , flavor = dimsum.flavor(ingredients)
-			  , i = 0
-			  , len = flavor.length;
+            var ingredients = "Space, the final frontier. These are the voyages of the starship Enterprise. Her five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before."
+              , flavor = dimsum.flavor(ingredients)
+              , i = 0
+              , len = flavor.length;
 
-			for (i; i < len; i++) {
-				assert.ok(ingredients.match( new RegExp(flavor[i], 'i') ));
-			}
+            for (i; i < len; i++) {
+                assert.ok(ingredients.match( new RegExp(flavor[i], 'i') ));
+            }
 
-		});
+        });
 
-		it('Registers a new flavor when given a key and a string', function() {
+        it('Registers a new flavor when given a key and a string', function() {
 
-			var key = 'spock'
-			  , ingredients = "Space, the final frontier. These are the voyages of the starship Enterprise. Her five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before."
-			  , flavor = dimsum.flavor(key, ingredients)
-			  , result = dimsum.flavor(key)
-			  , i = 0
-			  , len = flavor.length;
+            var key = 'spock'
+              , ingredients = "Space, the final frontier. These are the voyages of the starship Enterprise. Her five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before."
+              , flavor = dimsum.flavor(key, ingredients)
+              , result = dimsum.flavor(key)
+              , i = 0
+              , len = flavor.length;
 
-			for (i; i < len; i++) {
-				assert.ok(result.indexOf( flavor[i] ) > -1);
-			}
+            for (i; i < len; i++) {
+                assert.ok(result.indexOf( flavor[i] ) > -1);
+            }
 
-		});
+        });
 
-	});
+    });
 
-	/* Utils */
+    /* Utils */
 
-	var punct = [',','.',';',':','?','!']
-	  , punct_reg = new RegExp('[' + punct.join('') + ']*','g');
+    var punct = [',','.',';',':','?','!']
+      , punct_reg = new RegExp('[' + punct.join('') + ']*','g');
 
-	function normify(strings) {
-		return strings.join(' ')
-				.toLowerCase()
-				.replace(punct_reg, '');
-	};
+    function normify(strings) {
+        return strings.join(' ')
+                .toLowerCase()
+                .replace(punct_reg, '');
+    };
 
 });
